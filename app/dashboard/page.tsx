f12cfa49a5018c1740fb7adf97f09064d94ed486
Edgar Benavides <edgarben27@gmail.com>
2024-06-11 19:07:53 -0600

    refact: Delete auth

--- app/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import Section from "@/components/ui/section";
-
-export default function Dashboard() {
-  return (
-    <Section title="Dashboard">
-      <p>This is the dashboard page</p>
-    </Section>
-  );
-}
--- app/dashboard/profile/page.tsx
+++
@@ -1,9 +0,0 @@
-import Section from "@/components/ui/section";
-
-export default function ProfilePage() {
-  return (
-    <Section title="Profile">
-      <p>This is the user profile page</p>
-    </Section>
-  );
-}
